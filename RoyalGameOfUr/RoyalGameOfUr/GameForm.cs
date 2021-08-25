c4b638dbbe8fd88c17480cb75a16d8ab1d6139f3
kybersutr <kybersutr@gmail.com>
2021-08-25 14:42:31 +0200

    feat: add GameForm

---
+++ RoyalGameOfUr/RoyalGameOfUr/GameForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RoyalGameOfUr
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
